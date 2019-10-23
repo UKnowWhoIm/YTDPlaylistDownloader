377af8b694d41d141cf74771ea629686aae09c0a
UKnowWhoIm <51323747+UKnowWhoIm@users.noreply.github.com>
2019-10-24 00:59:01 +0530

    works perfectly with head, buggy in headless

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
 from selenium import webdriver
+from time import sleep
+from selenium.webdriver.chrome.options import Options  
 
-browser = webdriver.Chrome()
+chrome_options = Options()
+# chrome_options.add_argument("--headless") -> Buggy
 
-def Download(link):
-    print("Link",link)
+browser = webdriver.Chrome(options=chrome_options)
 
+def Download(r_link):
+    # Website Used : https://youtubedownload.video/youtube-to-mp3-downloader.php
+    
+    browser.get("https://youtubedownload.video/youtube-to-mp3-downloader.php")
+    while True:
+        # Keep On Trying Until element loads
+        print("Waiting For DDoSCheck")
+        try:
+            target1 = browser.find_element_by_css_selector("#urlYoutube")
+            break
+        except:
+            sleep(1)
+            pass
+    browser.execute_script("arguments[0].value=arguments[1]",target1,r_link)
+    browser.find_element_by_css_selector("#submit1").click()
+    while True:
+        # Keep On Trying Until element loads
+        print("Waiting For Search Result")
+        try:
+            browser.find_element_by_css_selector("input[type=button]").click()
+            break
+        except Exception as E:
+            sleep(1)
+            pass
+    while True:
+        # Keep On Trying Until element loads
+        print("Waiting For Conversion")
+        try:
+            browser.find_element_by_css_selector("input[value=Download]").click()
+            break
+        except:
+            sleep(1)
+            pass
+    
+    
+    
 link = input("Playlist Link? ")
 browser.get(link)
-raw_list = browser.find_element_by_css_selector("ytd-playlist-video-renderer")
-for i in range(len(raw)):
+raw_list = browser.find_elements_by_css_selector("ytd-playlist-video-renderer")
+for i in range(len(raw_list)):
     raw_list[i].click()
     Download(browser.current_url)
+    print("%i Done "%(i+1))
+    sleep(5)
     browser.get(link)
-    raw_list = browser.find_element_by_css_selector("ytd-playlist-video-renderer")
+    raw_list = browser.find_elements_by_css_selector("ytd-playlist-video-renderer")
     
 
 
